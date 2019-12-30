7e6eb6c45ef9e478c0205b95f36d22c0568cb211
Devin Jacoviello <jacoviello.devin@gmail.com>
2019-12-30 00:39:09 -0800

    site ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,39 @@
+// gulp
 var gulp = require('gulp')
-var sass = require('gulp-sass')
-var cleanCss = require("gulp-clean-css")
-var sourcemaps = require("gulp-sourcemaps")
 
+// css
+var cleanCss = require("gulp-clean-css")
+var postcss = require ("gulp-postcss")
+var sourcemaps = require("gulp-sourcemaps")
+var concat = require("gulp-concat")
+
+// browser refresh
 var browserSync = require('browser-sync')
 
+// images
 var imagemin = require("gulp-imagemin")
 
+// github
 var ghpages = require("gh-pages")
 
 
-sass.compiler = require('node-sass');
-
-gulp.task("sass", function () {
-    // we want to run "sass css/app.css app.css --watch"
-    return gulp.src("src/css/app.scss")
+gulp.task("css", function () {
+    return gulp.src([
+      "src/css/reset.css",
+      "src/css/typography.css",
+      "src/css/app.css"
+    ])
       .pipe(sourcemaps.init())
-      .pipe(sass())
+      .pipe(
+          postcss([
+            require("autoprefixer"),
+            require("postcss-preset-env")({
+              stage: 1,
+              browser: ["IE 11", "last 2 versions"]
+            })
+          ])
+      )
+      .pipe(concat("app.css"))
       .pipe(
         cleanCss({
           compatibility: 'ie8'
@@ -28,7 +45,7 @@ gulp.task("sass", function () {
 })
 
 gulp.task("html", function () {
-  return gulp.src("src/@.html")
+  return gulp.src("src/*.html")
     .pipe(gulp.dest("dist"))
 })
 
@@ -52,8 +69,8 @@ gulp.task("watch", function () {
   })
 
 
-  gulp.watch("src/@.html", ["html"]).on("change", browserSync.reload)
-  gulp.watch("src/css/app.scss", ["sass"])
+  gulp.watch("src/*.html", ["html"]).on("change", browserSync.reload)
+  gulp.watch("src/css/*.css", ["css"])
   gulp.watch("src/fonts/*", ["fonts"])
   gulp.watch("src/img/*", ["images"])
 })
@@ -62,4 +79,4 @@ gulp.task("deploy", function () {
   ghpages.publish("dist")
 })
 
-gulp.task('default', ["html", "sass", "fonts", "images", "watch"])
+gulp.task('default', ["html", "css", "fonts", "images", "watch"])
